932eded632f56566d72eb06b019a390ce3e44989
JZU0 <ljzh1030@gmail.com>
2023-11-19 18:13:31 +0900

    [#4] feat : add axiosinstance

---
+++ src/apis/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: `http://${process.env.REACT_APP_ENDPOINT}`,
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
