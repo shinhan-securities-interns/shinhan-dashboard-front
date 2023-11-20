cfd985ea0bcafd0345a55f11f65ab886ab956499
JZU0 <ljzh1030@gmail.com>
2023-11-20 23:41:08 +0900

    [#7] feat : add recoil

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { RecoilRoot } from 'recoil';
 import Main from './pages/Main';
+
 import './styles/reset.css';
 
 function App() {
-  return <Main />;
+  return (
+    <RecoilRoot>
+      <Main />
+    </RecoilRoot>
+  );
 }
 
 export default App;
---
+++ src/store/atoms.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const stockItemsState = atom({
+  key: 'stockItemsState',
+  default: '',
+});
