f53d2e1cdfcb69dbe132f60d4bb96cef057d2d2b
JZU0 <ljzh1030@gmail.com>
2023-11-22 08:17:34 +0900

    [#4] feat : 코스피 코스닥 추가

--- src/components/MiniHeader/index.jsx
+++ src/components/MiniHeader/index.jsx
@@ -1,11 +1,114 @@
-import React from 'react';
-import { Container, Image } from './styled';
+import React, { useEffect, useState } from 'react';
+import { axiosInstance } from '../../apis';
+import {
+  Container,
+  SlideContainer,
+  SlideWrapper,
+  Slide,
+  TextWrapper,
+  Image,
+  ImageWrapper,
+} from './styled';
 import logo from '../../assets/images/MYS2D.svg';
 
 const MiniHeader = () => {
+  const [data, setData] = useState(null);
+  const [predict, setPredict] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axiosInstance.get(`/kospi/kosdaq`);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchPredict = async () => {
+    try {
+      const response = await axiosInstance.get(`/prediction/kospi/kosdaq`);
+      setPredict(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const resetURL = () => {
+    const currentUrl = window.location.href;
+    const updatedUrl = currentUrl.split('?')[0];
+    window.location.href = updatedUrl;
+  };
+
+  useEffect(() => {
+    fetchPredict();
+    setInterval(() => {
+      //fetchData();
+    }, 300000000);
+  }, []);
+
+  const isDataAndPredictAvailable = data && predict;
+  console.log(data);
   return (
     <Container>
-      <Image src={logo} alt="logo" />
+      {isDataAndPredictAvailable && (
+        <SlideContainer>
+          <SlideWrapper>
+            <Slide original>
+              <TextWrapper>코스피</TextWrapper>
+              <TextWrapper>{data.kospi_now}</TextWrapper>
+              <TextWrapper
+                color={data.kospi_ratio.includes('+') ? 'red' : 'blue'}
+              >
+                {data.kospi_num}
+              </TextWrapper>
+              <TextWrapper
+                color={data.kospi_ratio.includes('+') ? 'red' : 'blue'}
+              >
+                {data.kospi_ratio}
+              </TextWrapper>
+              전망 예측
+              <TextWrapper
+                color={
+                  predict.kospi_prediction.includes('상승') ? 'red' : 'blue'
+                }
+              >
+                {predict.kospi_prediction}
+              </TextWrapper>
+            </Slide>
+            <Slide>
+              코스닥
+              <TextWrapper>{data.kosdaq_now}</TextWrapper>
+              <TextWrapper
+                color={data.kosdaq_ratio.includes('+') ? 'red' : 'blue'}
+              >
+                {data.kosdaq_num}
+              </TextWrapper>
+              <TextWrapper
+                color={data.kosdaq_ratio.includes('+') ? 'red' : 'blue'}
+              >
+                {data.kosdaq_ratio}
+              </TextWrapper>
+              전망 예측
+              <TextWrapper
+                color={
+                  predict.kosdaq_prediction.includes('상승') ? 'red' : 'blue'
+                }
+              >
+                {predict.kosdaq_prediction}
+              </TextWrapper>
+            </Slide>
+          </SlideWrapper>
+        </SlideContainer>
+      )}
+      <ImageWrapper>
+        <Image
+          onClick={() => {
+            resetURL();
+          }}
+          src={logo}
+          alt="logo"
+        />
+      </ImageWrapper>
     </Container>
   );
 };
--- src/components/MiniHeader/styled.js
+++ src/components/MiniHeader/styled.js
@@ -1,13 +1,92 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
   height: 10%;
   display: flex;
+  align-items: center;
   justify-content: flex-end;
 `;
 
-export const Image = styled.img`
+export const SlideContainer = styled.div`
   width: 50%;
+  height: 60%;
+  display: flex;
+  justify-content: flex-end;
+  overflow: hidden;
+  font-size: 1.2rem;
+`;
+
+export const SlideWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
+
+export const slideAnimation1 = keyframes`
+    0% {
+        transform: translateY(0%);
+    }
+    50% {
+        transform: translateY(-100%);
+    }
+    50.1% {
+        transform: translateY(100%);
+    }
+    100% {
+        transform: translateY(0%);
+    }
+`;
+
+export const slideAnimation2 = keyframes`
+    0% {
+        transform: translateY(0%);
+    }
+    100% {
+        transform: translateY(-200%);
+    }
+`;
+
+export const Slide = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: nowrap;
+  position: relative;
+  padding: 40px 0;
+  animation: 10s linear infinite normal none running
+    ${(props) => (props.original ? slideAnimation1 : slideAnimation2)};
+  animation-play-state: running
+
+  &::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #bbb;
+    z-index: 1;
+  }
+`;
+
+export const TextWrapper = styled.p`
+  color: ${(props) =>
+    props.color === 'red' ? 'red' : props.color === 'blue' ? 'blue' : 'black'};
+`;
+
+export const ImageWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Image = styled.img`
+  width: 15vw;
   height: auto;
+  margin-left: 20%;
 `;
